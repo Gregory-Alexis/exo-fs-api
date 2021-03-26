6a7068fb60e60b062a4491067ac1b3b284913898
Westindiess <gregoryalexiss@gmail.com>
2021-03-26 15:36:59 +0100

    exo-fs-api

---
+++ wc.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+
+if (process.argv.length < 3 || process.argv.length > 5) {
+  console.log('Usage: node cat.js file.txt')
+  process.exit(1)
+}
+
+if (!fs.existsSync(process.argv[2])) {
+  console.log('Error: file does not exist')
+  process.exit(1)
+}
+
+if (!fs.statSync(process.argv[2]).isFile()) {
+  console.log('Error: bad input')
+  process.exit(1)
+}
+
+let text = fs.readFileSync(process.argv[2], 'utf-8')
+
+if (process.argv.length === 4 && process.argv[3] !== '-l' && process.argv.length === 4 && process.argv[3] !== '-w' && process.argv.length === 4 && process.argv[3] !== '-c') {
+  console.log("cet argument d'inversement de pyramid est incorrecte")
+  process.exit(1)
+}
+
+let res = []
+
+if (process.argv[3] === '-l') {
+  res.push(text.split('\n').length)
+
+} else if (process.argv[3] === '-w') {
+  res.push(text.split(' ').length)
+} else if (process.argv[3] === '-c') {
+  res.push(text.split('').length)
+} else {
+  res.push(text.split('\n').length)
+  res.push(text.split(' ').length)
+  res.push(text.split('').length)
+}
+
+const resStr = res.join(' ')
+console.log(resStr)
+
