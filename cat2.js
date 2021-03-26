400e7565a463e6aa3787bd0c46f48e7c0dd2d09b
Westindiess <gregoryalexiss@gmail.com>
2021-03-26 18:01:56 +0100

    exo-fs-api

--- cat2.js
+++ cat2.js
@@ -5,36 +5,18 @@ if (process.argv.length < 3 && process.argv.length > 5) {
   process.exit(1)
 }
 
-if (!fs.existsSync(process.argv[2])) {
-  console.log(`${process.argv[2]} does not exist`)
-  process.exit(1)
-} else if (!fs.existsSync(process.argv[3])) {
-  console.log(`${process.argv[3]} does not exist`)
-  process.exit(1)
-} else if (!fs.existsSync(process.argv[4])) {
-  console.log(`${process.argv[4]} does not exist`)
-  process.exit(1)
-}
+let res = []
 
-const stats = fs.statSync(process.argv[2])
-const stats1 = fs.statSync(process.argv[3])
-const stats2 = fs.statSync(process.argv[4])
-
-if (!stats.isFile(process.argv[2])) {
-  console.log(`${process.argv[2]} is not a file`)
-  process.exit(1)
+for (let i = 2; i < process.argv.length; i++) {
+  if (!fs.statSync(process.argv[i]).isFile()) {
+    console.log(`${process.argv[i]} is not a file`)
+    process.exit(1)
+  }
+  if (!fs.existsSync(process.argv[i])) {
+    console.log(`${process.argv[i]} does not exist`)
+    process.exit(1)
+  }
+  res.push(fs.readFileSync(process.argv[i], 'utf-8'))
 }
-if (!stats1.isFile(process.argv[3])) {
-  console.log(`${process.argv[3]} is not a file`)
-  process.exit(1)
-}
-if (!stats2.isFile(process.argv[4])) {
-  console.log(`${process.argv[4]} is not a file`)
-  process.exit(1)
-}
-
-const str = fs.readFileSync(process.argv[2], 'utf-8')
-const str1 = fs.readFileSync(process.argv[3], 'utf-8')
-const str2 = fs.readFileSync(process.argv[4], 'utf-8')
-
-console.log(`${str}${str1}${str2}`)
+const resStr = res.join(' ')
+console.log(resStr)
