c3a59bc4a18d6ca3c79da456cf40519100286a0c
Westindiess <gregoryalexiss@gmail.com>
2021-03-27 19:45:30 +0100

    exo-fs-api

--- tail.js
+++ tail.js
@@ -7,20 +7,20 @@ if (process.argv.length !== 3 && process.argv.length !== 5) {
 }
 
 if (process.argv.length === 5 && !fs.existsSync(process.argv[4])) {
-  console.log('Error: file does not exist')
+  console.log(`Error: ${process.argv[4]} file does not exist`)
   process.exit(1)
 }
 if (process.argv.length === 5 && !fs.statSync(process.argv[4]).isFile()) {
-  console.log('Error: This is not a file')
+  console.log(`Error: ${process.argv[4]} This is not a file`)
   process.exit(1)
 }
 
 if (process.argv.length === 3 && !fs.existsSync(process.argv[2])) {
-  console.log('Error: file does not exist')
+  console.log(`Error: ${process.argv[2]} file does not exist`)
   process.exit(1)
 }
 if (process.argv.length === 3 && !fs.statSync(process.argv[2]).isFile()) {
-  console.log('Error: This is not a file')
+  console.log(`Error: ${process.argv[2]} This is not a file`)
   process.exit(1)
 }
 
@@ -30,7 +30,7 @@ if (process.argv.length === 5 && process.argv[2] !== '-n') {
 }
 
 if (process.argv.length === 5 && isNaN(process.argv[3])) {
-  console.log('Error: This is not a number')
+  console.log(`Error: ${process.argv[3]} is not a number`)
   process.exit(1)
 }
 
@@ -38,8 +38,8 @@ if (process.argv.length === 5) {
   const read = fs.readFileSync(process.argv[4], 'utf-8')
   const txt = read.split('\n').splice(process.argv[3])
 
-  if (process.argv[3] > read.length) {
-    console.log('Error: You cannot split more than the length text')
+  if (process.argv[3] >= read.split('\n').length) {
+    console.log(`Error: You cannot split more or equal than the length text. Max length limit is: ${read.split('\n').length}`)
     process.exit(1)
   }
   numStr = Number(process.argv[3])
