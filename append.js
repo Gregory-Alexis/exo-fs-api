f96aef1fa020ab7087c01b828d1e71d1db2ed6be
Westindiess <gregoryalexiss@gmail.com>
2021-03-28 15:06:32 +0200

    exo-fs-api

--- append.js
+++ append.js
@@ -1,28 +1,26 @@
 const fs = require('fs')
 
-if (process.argv.length < 4) {
-  console.log('Usage: node cat.js file.txt')
+if (process.argv.length < 3) {
+  console.log('Usage: node append.js file1.txt file2.txt file3.txt copyFile.txt')
   process.exit(1)
 }
 
 for (let i = 2; i < process.argv.length - 1; i++) {
   if (!fs.existsSync(process.argv[i])) {
     console.log(`Error: ${process.argv[i]} does not exist`)
+    process.exit(1)
   }
   if (!fs.statSync(process.argv[i]).isFile()) {
     console.log(`Error: ${process.argv[i]} is not a file`)
+    process.exit(1)
   }
 }
 const res = []
 
-const txt = fs.readFileSync(process.argv[2], 'utf-8')
-const txt2 = fs.readFileSync(process.argv[3], 'utf-8')
-const txt3 = fs.readFileSync(process.argv[4], 'utf-8')
-
-
-const txtStr = res.push(txt.split('\n'))
-const txtStr2 = res.push(txt2.split('\n'))
-const txtStr3 = res.push(txt3.split('\n'))
+for (let i = 2; i < process.argv.length - 1; i++) {
+  const txt = fs.readFileSync(process.argv[i], 'utf-8')
+  res.push(txt.split('\n'))
+}
 
 const resStr = res.join(' ')
 fs.writeFileSync(process.argv[5], resStr)
